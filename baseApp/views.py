233f8f3db1951640ede7d8ae8bcfabafdf604451
luzhiyuan <yuanbao0228@gmail.com>
2017-12-28 16:48:47 +0800

    首页验证cookie

--- baseApp/views.py
+++ baseApp/views.py
@@ -6,7 +6,11 @@ from tools import *
 # Create your views here.
 def index(request):
     # cookie 验证
-    return render(request, 'baseApp/index.html')
+    if 'Username' in request.COOKIES:
+        username = request.COOKIES.get('Username', '')
+        if username:
+            return render(request, 'baseApp/index.html')
+    return redirect('login_with_args', state='0')
 
 def login(request, **kwargs):
     state = ''
@@ -23,10 +27,7 @@ def checkLogin(request):
         isuser = getUserPassword(username,password)
         if ("1" == isuser):
             # 写cookie
-            response = redirect('index')
-            response.set_cookie(
-                'Username', username, expires=datetime.datetime.now() +datetime.timedelta(days=3))
-            return response
+            return writeCookie(request,username)
         else:
             # 提示账号密码错误
             return redirect('login_with_args', state='1')
@@ -40,4 +41,11 @@ def registerIn(request):
     userdata = LoginDatas(Username=username, Password=password)
     userdata.save()
     # 写cookie
-    return render(request, 'baseApp/index.html')
+    return writeCookie(request, username)
+
+def writeCookie(request,username):
+    # 写cookie 跳转到首页
+    response = redirect('index')
+    response.set_cookie(
+        'Username', username, expires=datetime.datetime.now() + datetime.timedelta(days=3))
+    return response
