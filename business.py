33876f1d7cb76860d27536b62cb660d53072f11a
luzhiyuan <yuanbao0228@gmail.com>
2018-01-17 17:53:59 +0800

    调整业务后台基本逻辑

--- business.py
+++ business.py
@@ -1,5 +1,8 @@
 def getFirstSightPlan(userqueryinfo,userinfo):
-
+    disname = userqueryinfo['disname']
+    if (userinfo):
+        gender = userinfo[0].gender
+    # 根据用户已有信息获取"初视"治疗方案
     message = {
         'recCode': "successed",
         'openid': ''
